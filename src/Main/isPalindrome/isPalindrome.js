0c1f49ffae4087d6b839cf14217c0a4eeca7a528
Andrew <arhorn@smcm.edu>
2019-01-23 15:23:56 +0700

    clarified isPalindrome function

--- src/Main/isPalindrome/isPalindrome.js
+++ src/Main/isPalindrome/isPalindrome.js
@@ -1,13 +1,16 @@
-const isPalindrome = possiblePalindrom => {
-  const onlyLetters = possiblePalindrom
+const isPalindrome = possiblePalindrome => {
+  const onlyLettersAndNumbers = possiblePalindrome // Takes out punctuation, and anything that's not a letter or number
     .match(/[a-zA-Z0-9]+/g)
     .join('')
     .toLowerCase();
 
-  const max = Math.floor(onlyLetters.length / 2);
+  const max = Math.floor(onlyLettersAndNumbers.length / 2);
 
   for (let i = 0; i < max; i++) {
-    if (onlyLetters[i] !== onlyLetters[onlyLetters.length - 1 - i]) {
+    if (
+      onlyLettersAndNumbers[i] !==
+      onlyLettersAndNumbers[onlyLettersAndNumbers.length - 1 - i]
+    ) {
       return false;
     }
   }
